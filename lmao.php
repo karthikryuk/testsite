86bd4bf3ae091d6340e3f5db2ad6efc57c393db7
karthikryuk <128225337+karthikryuk@users.noreply.github.com>
2023-03-18 16:39:01 +0530

    Add files via upload

---
+++ lmao.php
@@ -0,0 +1,45 @@
+<?php
+
+$name = $_POST["name"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+$city = filter_input(INPUT_POST, "city", FILTER_VALIDATE_INT);
+$terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOL);
+
+if ( ! $terms) {
+    die("Terms must be accepted");
+}   
+
+$host = "localhost";
+$dbname = "message_db";
+$username = "root";
+$password = "";
+        
+$conn = mysqli_connect(hostname: $host,
+                       username: $username,
+                       password: $password,
+                       database: $dbname);
+        
+if (mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_error());
+}           
+        
+$sql = "INSERT INTO message (name, password, email, city)
+        VALUES (?, ?, ?, ?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+ 
+    die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "sssi",
+                       $name,
+                       $password,
+                       $email,
+                       $city);
+
+mysqli_stmt_execute($stmt);
+
+echo "Record saved.";
